2c3db1291ce4fd38f88ccf742222116a0aca4ca7
Vladimir Ageev <vladimir.ageev@me.com>
2016-10-15 03:47:29 +0300

    Some comments deleted

--- CafeMenu/CategoryViewController.swift
+++ CafeMenu/CategoryViewController.swift
@@ -71,54 +71,9 @@ class CategoryViewController: UIViewController {
                         }
                     }
                     }
-                
-
             }
         
-        
-//        var data :NSData? =  FileUtility.dataFromPath("data") as? NSData
-//        let bundle = Bundle.mainBundle
-//        let path = bundle.pathForResource("data", ofType: "json")
-//        let content = NSString.stringWithContentsOfFile(path) as String
-//        print(content) // prints the content of data.json
-        
-        
-        
-        
-        let category1 = CategoryItem(category: .bakery)
-        let item1 = MenuItem()
-        item1.name = "Muffin"
-        item1.descriptionText = "I am a great muffin"
-        item1.picture = #imageLiteral(resourceName: "muffin")
-        category1.items.append(item1)
-        typesOfFood.append(category1)
-
-        
-        let category2 = CategoryItem(category: .cakes)
-        let item2 = MenuItem()
-        item2.name = "Cake"
-        item2.descriptionText = "I am a big cake"
-        item2.picture = #imageLiteral(resourceName: "cake")
-        category2.items.append(item2)
-        typesOfFood.append(category2)
-        
-        let category3 = CategoryItem(category: .coffee)
-        let item3 = MenuItem()
-        item3.name = "Espresso"
-        item3.descriptionText = "I am a strong coffee"
-        item3.picture = #imageLiteral(resourceName: "coffee")
-        category3.items.append(item3)
-        typesOfFood.append(category3)
-        
-        let category4 = CategoryItem(category: .food)
-        let item4 = MenuItem()
-        item4.name = "Sandwich"
-        item4.descriptionText = "I have a fish inside of me!"
-        item4.picture = #imageLiteral(resourceName: "sandwich")
-        category4.items.append(item4)
-        typesOfFood.append(category4)
-        
-            }
+    }
 }
 
 
